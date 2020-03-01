c89282849fd6b51f1a7aba462128b6f0fb2d998c
Kubion <jakub98kowalski@gmail.com>
2020-03-01 18:56:08 +0100

    Pierwszy commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    public class Program
+    {
+        private const string MatchEmailPattern =
+            @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+            + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+            + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+            + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+        
+        public static async Task Main(string[] args)
+        {
+            var emails = await GetEmails(args[0]);
+
+            foreach (var email in emails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+
+        private static async Task<IEnumerable<string>> GetEmails(string url)
+        {
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            var stringContent = await response.Content.ReadAsStringAsync();
+
+            var emailRegex = new Regex(MatchEmailPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var matches = emailRegex.Matches(stringContent);
+
+            return matches.Select(m => m.ToString());
+        }
+    }
+}
